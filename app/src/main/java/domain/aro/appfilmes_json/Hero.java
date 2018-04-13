6af53546a5bb884465f74867ece38d2d903d2133
alessandro.roliveir4 <alessandrorochadeoliveira@hotmail.com>
2018-04-13 09:31:19 -0300

    Initial commit

---
+++ app/src/main/java/domain/aro/appfilmes_json/Hero.java
@@ -0,0 +1,63 @@
+package domain.aro.appfilmes_json;
+
+/**
+ * Created by alessandro.roliveir4 on 13/04/2018.
+ */
+
+
+public class Hero {
+
+    private int id;
+    private String name, realname;
+    private int rating;
+    private String teamaffiliation;
+
+    public Hero(int id, String name, String realname, int rating, String teamaffiliation) {
+        this.id = id;
+        this.name = name;
+        this.realname = realname;
+        this.rating = rating;
+        this.teamaffiliation = teamaffiliation;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getTeamaffiliation() {
+        return teamaffiliation;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRealname(String realname) {
+        this.realname = realname;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public void setTeamaffiliation(String teamaffiliation) {
+        this.teamaffiliation = teamaffiliation;
+    }
+}
